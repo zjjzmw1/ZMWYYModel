d7d365e92a471c4e2fd03ace6bfb98c9761b316c
zhangmingwei <zjjzmw1@163.com>
2016-07-21 14:36:17 +0800

    Initial Commit

---
+++ ZMWYYModel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZMWYYModel
+//
+//  Created by speedx on 16/7/21.
+//  Copyright © 2016年 speedx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
